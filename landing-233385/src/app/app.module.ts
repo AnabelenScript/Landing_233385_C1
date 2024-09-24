368afbcad6fb849af123275370df9bf45249bb3f
AnabelenScript <ana0507belen@gmail.com>
2024-09-24 14:46:22 -0600

    Main view complete with 3 components and style

--- landing-233385/src/app/app.module.ts
+++ landing-233385/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainModule } from '../main/main.module';
+import { NavbarComponent } from '../main/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
